39917130966359b82843130b4d00d09105eb3f14
Jordan Kwek <jordankwek26@gmail.com>
2021-12-24 21:42:18 +0800

    new ui

--- script.js
+++ script.js
@@ -2,17 +2,12 @@
 const choices = Array("rock","paper","scissors") 
 let choice = choices[Math.floor(Math.random()*choices.length)];
 
-function play() {
+function computerPlay() {
     return choice;
 }
 
-function computerPlay() {
-    return play();
-}
 
-
-function playRound(computerChoice) {
-    playerInput = prompt("What is your choice?")
+function playRound(computerChoice,playerInput) {
     let playerChoice = playerInput.toLowerCase();
     if (playerChoice == "rock" && computerChoice == "scissors") {
         return "You win! Rock beats Scissors"
@@ -33,15 +28,40 @@ function playRound(computerChoice) {
         return "You win! Scissors beats Rock"
     }    
     else if (playerChoice == "paper" && computerChoice == "rock") {
-        return "You win! Paper beats rock"
+        return "You win!"
     }    
 }
-function game() {
-    console.log(playRound(computerChoice))
-    console.log(playRound(computerChoice))
 
-}
-  
 
-const computerChoice = computerPlay();
-game()
+
+rockChoice = document.getElementById("button-R");
+rockChoice.addEventListener("click",() => {
+    playerInput = "rock";
+    computerResults = computerPlay();
+    results = playRound(computerResults,playerInput);
+    console.log(computerResults)
+    let gameResults = document.createElement("div");
+    gameResults.innerHTML = results;
+    document.getElementsByClassName("bottom-container")[0].appendChild(gameResults);
+});
+
+
+scissorsChoice = document.getElementById("button-S");
+scissorsChoice.addEventListener("click",() => {
+    playerInput = "scissors";
+    computerResults = computerPlay();
+    results = playRound(computerResults,playerInput)
+    let gameResults = document.createElement("div");
+    gameResults.innerHTML = results;
+    document.getElementsByClassName("bottom-container")[0].appendChild(gameResults);
+});
+
+paperChoice = document.getElementById("button-P");
+paperChoice.addEventListener("click",() => {
+    playerInput = "paper";
+    computerResults = computerPlay();
+    results = playRound(computerResults,playerInput)
+    let gameResults = document.createElement("div");
+    gameResults.innerHTML = results;
+    document.getElementsByClassName("bottom-container")[0].appendChild(gameResults);
+});
