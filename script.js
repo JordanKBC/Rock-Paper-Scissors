85d320cbd937f0122610d4a538f716ef22be6a72
Jordan Kwek <jordankwek26@gmail.com>
2022-02-08 16:27:15 +0800

    Created a GUI for the game

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 
 // This function takes the user input and returns it as a capitalised string
 function playerPlay() {
-    rawChoice = prompt("What is your choice?")
+    rawChoice 
     rawChoiceFirstChar = (rawChoice.split(""))[0];
     formattedChoice = rawChoiceFirstChar.toUpperCase()
     for (i=1; i < rawChoice.length; i++) {
@@ -17,12 +17,16 @@ function computerPlay() {
     return choices[index];
 }
 
+let playerChoice = playerPlay();
+let computerChoice = computerPlay();
+
 //This function decides if the computer or the player wins
 function playRound(playerSelection,computerSelection) {
     if (playerSelection == computerSelection) {
         return "It is a Draw"
     }
-    else if (playerSelection == "Rock" && computerSelection == "Scissors" ||playerSelection == "Scissors" && computerSelection == "Paper"||playerSelection == "Paper" && computerSelection == "Rock" ) {
+    else if (playerSelection == "Rock" && computerSelection == "Scissors" ||playerSelection == "Scissors" && computerSelection == "Paper"
+            ||playerSelection == "Paper" && computerSelection == "Rock" ) {
         return `Player Wins. ${playerSelection} beats ${computerSelection}`
     }
     else {
@@ -30,17 +34,7 @@ function playRound(playerSelection,computerSelection) {
     }
 }
 
-//This function plays a certain number of rounds of the game
-function game() {
-    for (let i=0; i < 4; i++) {
-        let playerChoice = playerPlay();
-        let computerChoice = computerPlay();
-        if (playerChoice != undefined && computerChoice != undefined) {
-            console.log(playRound(playerChoice,computerChoice));
-        }
-    }
-}
 
-game();
+
 
 
